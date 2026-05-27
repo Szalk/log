536f3afae41c3601e67d2a11388114762253ebdd
Szalk <szalokadam@gmail.com>
2026-05-27 23:22:03 +0200

    Add files via upload

---
+++ combined.changelog.js
@@ -0,0 +1,29 @@
+const CHANGELOG_COMBINED = {
+  "exportedAt": "2026-05-27T21:20:42.396Z",
+  "apps": {
+    "nstart": {
+      "meta": {
+        "appSlug": "nstart",
+        "appName": "NSTART",
+        "latestVersion": "260527",
+        "exportedAt": "2026-05-27T21:20:42.398Z"
+      },
+      "encrypted": {
+        "iv": "oy1rdddwPc5yWwf1",
+        "data": "qwCsTcT8p46FZRPPV4GznRBWdpOIPy1+bJ4Vx1GZbGcJCURUryU3nDESVIyOidrIQYulQLRAlyAdVc/+Ol1aVjP/3FYGY5sbabA3EIYoCsxj5MSvAM+yUBgVj7Z8Izuh1NW0Jsbm6JDBeTgMvrA82Q9jgvzAvoV4/nZ/C6AhZdk5+brovDV+eazXplD3yJLm9nc8uQNv8UQzQIRl2iXKNC4MIXUjTcudDorwfwuYiIb4C+3q9GiCYDMGJ/8qVLBAPuxd91XwNz/At2FS7+4NNbH7BoZxbey8oIQA8S44rJbIv0kx+ITaqWe5Twf4noNQOG4ZkQMvcau9QMgAOj3Lyn+2ADd05+UIYMEGOo8Wn2hdBn7LxS4n2RNS0Nvvv+ypBIiWugyHcVfvDiY9td/QfoZX+qcLmuEE6dtPqrW9JXP5j2UIbCIhEmYb"
+      }
+    },
+    "maja": {
+      "meta": {
+        "appSlug": "maja",
+        "appName": "MAJA",
+        "latestVersion": "260527",
+        "exportedAt": "2026-05-27T21:20:42.400Z"
+      },
+      "encrypted": {
+        "iv": "uxBbXr5rlH27WtJi",
+        "data": "W1TZ0+z5oE3ixmcBBM7u0c6MOIhSzGu//ZHnJEmIfeIFF9INdiDePEspNKeu30iX4qvBVs4fQQKpGlcQlgCA6zrdVhxyXwQyJzgdppadkbTICHyWsDta3aSOY6MR7brGIgQlPi4oucR85A1QFL5hocKj0HZp3XKkYpVnn61POJez9ccri2wrqHtKoN+DS0jDbg=="
+      }
+    }
+  }
+};
