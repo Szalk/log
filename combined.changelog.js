6acd16735412aba20f449e59531f4e2e307a721b
Szalk <szalokadam@gmail.com>
2026-05-28 20:42:00 +0200

    Update combined.changelog.js

--- combined.changelog.js
+++ combined.changelog.js
@@ -1,14 +1,26 @@
 const CHANGELOG_COMBINED = {
-  "exportedAt": "2026-05-28T18:21:12.923Z",
+  "exportedAt": "2026-05-28T18:40:59.225Z",
   "apps": {
-    "navstart": {
+    "nstart": {
       "meta": {
-        "latestVersion": "260528",
-        "exportedAt": "2026-05-28T18:21:12.925Z"
+        "latestVersion": "260527",
+        "exportedAt": "2026-05-28T18:40:59.227Z",
+        "salt": "Y2hhbmdlbG9nXzIwMjYhIQ=="
       },
       "encrypted": {
-        "iv": "q0nsXecPmBKsYGCx",
-        "data": "eKhVTtYUqDPZ7iq1sABgtVkAckG5nlsjfIb63MGRb173s6DsblaKMUMwQQKdd6KAL8wabWaIaNSvHNBGOIxoRPQwg+cQJg/ZtThFCPjpbsrvdf6VbIOJqndfgkecVIhYar6yGpMpWh9cyW4MeEMvep3vczJaARAIDlvmmQdTgoT3rT/Qi3zakPc="
+        "iv": "VRwBZh1HsZa7CXpy",
+        "data": "bRC+n5DrCM/fX2aM0eaUhsN+0EcJHgtcAyAzxrCyih0CNyvh7XQy9Hqzs24RjYY2A+yW7cypHx3G93Iu93wBOUyZNSvTCIa/NndT6Y8qNKXhBV2TCDYPfbaJuQtJdw3J85IvivJoxm8/s3MvBpN8tcD+y9P3G6aOrYt9cLsNSfCoZd2+2DttNpGR9DTSWAQCJD9U9hYSnnawCQQBzrteQ8hwkYEuROu7u0h7tPsuk+kia8r3UNz1QA=="
+      }
+    },
+    "maja": {
+      "meta": {
+        "latestVersion": "260527",
+        "exportedAt": "2026-05-28T18:40:59.228Z",
+        "salt": "Y2hhbmdlbG9nXzIwMjYhIQ=="
+      },
+      "encrypted": {
+        "iv": "c3BhCTqAiT62yVZm",
+        "data": "dRSNFtn0WsgfFriBriIprAHEou8LRj1wtuk0tF3Z2pIYNc0rcP9wPrOFj7safqrPniPX9WrOir70iXs/qFsxu01rzXHJ/zlX6yKlWDBgWKX2oC9JqP24RJk2pu/VCGlmMYo2sQWil9NITh/TWIGmbka45WtbiSU7krSrUbaR/jGW8ptWp+jgPApkZoCWTKlORvQtGJ5/gv+7AfJC3hHTOA=="
       }
     }
   }
