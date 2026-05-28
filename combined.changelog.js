9ebb6defb097d618fe22f0c62a99213bb0fba5d2
Szalk <szalokadam@gmail.com>
2026-05-28 21:33:45 +0200

    Update combined.changelog.js

--- combined.changelog.js
+++ combined.changelog.js
@@ -1,24 +1,24 @@
 const CHANGELOG_COMBINED = {
-  "exportedAt": "2026-05-28T19:27:37.435Z",
+  "exportedAt": "2026-05-28T19:33:07.742Z",
   "apps": {
     "nstart": {
       "meta": {
-        "latestVersion": "260527",
-        "exportedAt": "2026-05-28T19:27:37.439Z"
+        "latestVersion": "260528",
+        "exportedAt": "2026-05-28T19:33:07.743Z"
       },
       "encrypted": {
-        "iv": "szbYHHjFNJmfcjAR",
-        "data": "wy6DCMdENO4c2Jq+tcSo7jJysjGoH5WwlIi5Iqnmq1MMuFlgLrsMOvXRQyGvMpWGH8GMxOqDOkbJrEH8R5FsuNTOsRyb+SENFoKPAG5Dz4uKGAmzuiB7bnVS6gyLV0dMqAVvX8Ya+VJM89mgzsGuucfqnAG5DLO+3pDI5pFBFB0Pa2hjBzxUqkKtG+DyW+zcJpy27hnpd85bc2mvbXnrki4v4vjG1tjdeRiIJklnGf47ACr5/azSOQ=="
+        "iv": "MnN5wmvA0dMlf9ub",
+        "data": "5jFhJwfo9Y7oPuvzLknklUCklxCNiFzKxGnsYds/6ezn4XrdAr9t5adLkLhfbBp0Z49qp8MYPzymXCUbvgb2dEWE/Z9BFO9S5UExOl483CEOiMSO7XRwSE/VBh51jF5K+4r6dLSXJLMTeyX2hwciaoJK9Cs57DJPds0ZCdFPSRCrv60+/1uZgpHZxbwGqzBz0hOkymd1Gjc6RYuKym7wGTYCGBcaMFL2YLvsruQQj7TiKnOdcBJNFQ2xEBoXPT3zUTRFieC5mFY79JBBptDmcyildIWygpZs35MQrZCP1rGNZwyeSF5/zFtfJMhf173rvhmZMpep4QN5NHgqbcMF0IGWpxIqg+df7tjGwnCUEa8/eMnkHZ6zJYzG1Ny1hyLvBMti/ByUnqX/ufNN"
       }
     },
     "maja": {
       "meta": {
         "latestVersion": "260527",
-        "exportedAt": "2026-05-28T19:27:37.441Z"
+        "exportedAt": "2026-05-28T19:33:07.744Z"
       },
       "encrypted": {
-        "iv": "H0OpxumT516l/bGu",
-        "data": "HzR4qswgEJFXmX2j1B32MzcWPztxR8VrOyXGZncoHNnCf26UfnCPQiCrYnz5tlr/Xv8i04leJ9wvT3Mpa7tYtQTvbHpNlBvI6y/Q5YGRZbtvPyKoHv/DaY1jps+uzk/1OWyiVDr6n+JEFy0nUJwEAQHk5JKRBEFKoyeY40K/3FlekW7EKsdFf0g/fm1vuiN2c7fb4Sy2f+4g3SQn/WkStg=="
+        "iv": "6zC8kQZh7w0m95B5",
+        "data": "0VV7Q+z2pDBaRsqfmyFK+gWwFqWAaEnslDg78ZdeY6Y4EXq8wHfba/D68LPofymYz9YYkpJX8rZlBLBtcDZtGMyt8ZDlYRxY4udBeHSIzQrc09ud3XTjpnINii8/lX8PKG721ag9FqeL9khKGXpe0zlNHeFTXX9URmO/mGbkkqJGw8pXPCRtbFMpX6gAKCKlTGE2k9gQZUuhaozqkpdAoQ=="
       }
     }
   }
